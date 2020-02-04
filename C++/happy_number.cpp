8a907eb390622c1bb4e4c14a2187906f4c0b051f
eric82714 <0958820714ab@gmail.com>
2020-02-04 21:05:48 +0800

    Create happy_number.cpp

---
+++ C++/happy_number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if (n == 1) return 1;
+        if (n == 4) return 0;
+        
+        int sum = 0;
+        
+        while (n != 0) {
+            sum += static_cast<int>(pow(n % 10, 2));
+            n /= 10;
+        }
+        
+        return isHappy(sum);
+    }
+};
